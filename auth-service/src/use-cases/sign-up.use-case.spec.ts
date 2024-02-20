4412d1b6e6d991a29481397dbd0d05052c4acc69
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2024-02-20 23:03:13 +0000

    test: should throw error if email input is null

--- auth-service/src/use-cases/sign-up.use-case.spec.ts
+++ auth-service/src/use-cases/sign-up.use-case.spec.ts
@@ -7,4 +7,18 @@ describe('Sign-Up Use Case', () => {
     const promise = sut.execute({ email: '' });
     await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
   });
+
+  it('should throw error if email input is "null"', async () => {
+    const sut = new SignUpUseCase();
+    const promise = sut.execute({ email: null } as any);
+    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
+  });
+  // caso o email seja indefinido
+  // caso o email não tenha @
+  // caso não tenha um dominio
+  // caso não tenha um endereço
+  // caso termine com um .
+  // caso tenha mais de um ponto consecutivo
+  // caso não tenha nenhum ponto depois do endereço
+  // caso ele tenha um espaço em branco
 });
