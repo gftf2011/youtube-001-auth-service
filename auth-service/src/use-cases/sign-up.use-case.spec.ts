17870904d05f29144585b4b9a477a838ac95d589
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2024-02-20 23:05:27 +0000

    test: should throw error if email input is undefined

--- auth-service/src/use-cases/sign-up.use-case.spec.ts
+++ auth-service/src/use-cases/sign-up.use-case.spec.ts
@@ -13,7 +13,12 @@ describe('Sign-Up Use Case', () => {
     const promise = sut.execute({ email: null } as any);
     await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
   });
-  // caso o email seja indefinido
+
+  it('should throw error if email input is "undefined"', async () => {
+    const sut = new SignUpUseCase();
+    const promise = sut.execute({} as any);
+    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
+  });
   // caso o email não tenha @
   // caso não tenha um dominio
   // caso não tenha um endereço
