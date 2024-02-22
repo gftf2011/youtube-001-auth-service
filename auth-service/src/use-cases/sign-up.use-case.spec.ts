544bdeadb9312b193e41dd0b25a262850a968875
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2024-02-22 23:27:43 +0000

    test: should throw error if first_name input has a surname with one character

--- auth-service/src/use-cases/sign-up.use-case.spec.ts
+++ auth-service/src/use-cases/sign-up.use-case.spec.ts
@@ -97,4 +97,13 @@ describe('Sign-Up Use Case', () => {
     });
     await expect(promise).rejects.toThrow(new InvalidFirstNameDomainError());
   });
+
+  it('should throw error if first_name has a surname with one character', async () => {
+    const sut = new SignUpUseCase();
+    const promise = sut.execute({
+      email: 'test@mail.com',
+      first_name: 'aa a',
+    });
+    await expect(promise).rejects.toThrow(new InvalidFirstNameDomainError());
+  });
 });
--- auth-service/src/use-cases/sign-up.use-case.ts
+++ auth-service/src/use-cases/sign-up.use-case.ts
@@ -12,16 +12,27 @@ import {
 const VALID_EMAIL_REGEX =
   /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
+const WHITE_SPACE_REGEX = /(\s)+/;
+
 export class SignUpUseCase implements ISignUpUseCase {
   async execute(input: ISignUpUseCase.INPUT): Promise<ISignUpUseCase.OUTPUT> {
     if (!VALID_EMAIL_REGEX.test(input.email)) {
       throw new InvalidEmailDomainError();
     }
 
-    const firstName = input.first_name;
+    const firstName = input.first_name
+      ? input.first_name
+          .trim()
+          .split(WHITE_SPACE_REGEX)
+          .map((value: string) => value.trim())
+          .join(' ')
+          .toLowerCase()
+      : '';
 
-    if (!firstName || firstName.length < 2) {
-      throw new InvalidFirstNameDomainError();
-    }
+    firstName.split(' ').forEach((value: string) => {
+      if (value || value.length < 2) {
+        throw new InvalidFirstNameDomainError();
+      }
+    });
   }
 }
