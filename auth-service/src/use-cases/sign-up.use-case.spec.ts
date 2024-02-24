c9f2d7702a22843cf0c67f998931cc77356978f3
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2024-02-24 21:02:13 +0000

    test: should throw error if last_name has only white spaces

--- auth-service/src/use-cases/sign-up.use-case.spec.ts
+++ auth-service/src/use-cases/sign-up.use-case.spec.ts
@@ -165,4 +165,14 @@ describe('Sign-Up Use Case', () => {
     });
     await expect(promise).rejects.toThrow(new InvalidLastNameDomainError());
   });
+
+  it('should throw error if last_name has only white spaces', async () => {
+    const sut = new SignUpUseCase();
+    const promise = sut.execute({
+      email: 'test@mail.com',
+      first_name: 'aa',
+      last_name: ' ',
+    });
+    await expect(promise).rejects.toThrow(new InvalidLastNameDomainError());
+  });
 });
