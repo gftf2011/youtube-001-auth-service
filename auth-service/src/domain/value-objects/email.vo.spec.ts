c843ef0929e78a40b571e6c241ff060605e17023
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2024-03-04 23:03:27 +0000

    refactor: move email validation tests to their own test file

---
+++ auth-service/src/domain/value-objects/email.vo.spec.ts
@@ -0,0 +1,54 @@
+import { InvalidEmailDomainError } from '../errors';
+import { Email } from './email.vo';
+
+describe('Email Value-Object', () => {
+  it('should throw error if email input is empty', () => {
+    const sut = () => new Email('');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input is "null"', () => {
+    const sut = () => new Email(null as any);
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input is "undefined"', () => {
+    const sut = () => new Email(undefined as any);
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input does not have an @', () => {
+    const sut = () => new Email('testmail.com');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input does not have a domain', () => {
+    const sut = () => new Email('@mail.com');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input does not have an address', () => {
+    const sut = () => new Email('test@.com');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input ends with a dot', () => {
+    const sut = () => new Email('test@mail.');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input has more than one consecutive dot', () => {
+    const sut = () => new Email('test@mail..com');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input has no dot between address', () => {
+    const sut = () => new Email('test@mail');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+
+  it('should throw error if email input has a white space', () => {
+    const sut = () => new Email('test@ mail.com');
+    expect(sut).toThrow(new InvalidEmailDomainError());
+  });
+});
--- auth-service/src/use-cases/sign-up.use-case.spec.ts
+++ auth-service/src/use-cases/sign-up.use-case.spec.ts
@@ -1,5 +1,4 @@
 import {
-  InvalidEmailDomainError,
   InvalidFirstNameDomainError,
   InvalidLastNameDomainError,
   InvalidPasswordDomainError,
@@ -7,66 +6,6 @@ import {
 import { SignUpUseCase } from './sign-up.use-case';
 
 describe('Sign-Up Use Case', () => {
-  it('should throw error if email input is empty', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: '' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input is "null"', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: null } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input is "undefined"', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({} as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input does not have an @', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'testmail.com' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input does not have a domain', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: '@mail.com' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input does not have an address', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'test@.com' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input ends with a dot', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'test@mail.' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input has more than one consecutive dot', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'test@mail..com' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input has no dot between address', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'test@mail' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
-  it('should throw error if email input has a white space', async () => {
-    const sut = new SignUpUseCase();
-    const promise = sut.execute({ email: 'test@ mail.com' } as any);
-    await expect(promise).rejects.toThrow(new InvalidEmailDomainError());
-  });
-
   it('should throw error if first_name input is "undefined"', async () => {
     const sut = new SignUpUseCase();
     const promise = sut.execute({ email: 'test@mail.com' } as any);
